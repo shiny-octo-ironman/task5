Numerically correct Forward-Backward

learn.py
@@ -54,6 +54,15 @@ def states_viterbi(values, transition_matrix, emission_matrix):
        
 # ---------------------------------------------------- Forward-Backward (ok) --
     
+def w_sum(w, axis=0):
+    # Infer shape for maximum weight, so it could correctly broadcast
+    shape = list(w.shape)
+    shape[axis] = 1
+    
+    # Use maximum weight as common multiplier
+    w_max = np.max(w, axis=axis)
+    return w_max + log(sum(exp(w - w_max.reshape(shape)), axis=axis))
+        
 def tail_weights(values, T, E, last_column):
     N = len(values)             
     W = zeros([N, N_STATES]) # [W]eights
@@ -64,7 +73,7 @@ def tail_weights(values, T, E, last_column):
         transition_cost = T
         target_cost = tile(W[i + 1], [N_STATES, 1])        
         cost = emission_cost + transition_cost + target_cost
-        W[i] = log(sum(exp(cost), axis=1))
+        W[i] = w_sum(cost, axis=1)
         
     return W
         
@@ -86,12 +95,12 @@ def posteriors(values, transition_matrix, emission_matrix):
     W = head_W[::-1] + tail_W - E[:, values].transpose()
    
     # Normalize to get posterior weights
-    W -= log(sum(exp(head_W[0])))
+    W -= w_sum(head_W[0])
     return W
     
 def states_forward_backward(values, transition_matrix, emission_matrix):
-    P = posteriors(values, transition_matrix, emission_matrix)
-    return argmax(P, 1)
+    W = posteriors(values, transition_matrix, emission_matrix)
+    return argmax(W, 1)
     
 # --------------------------------------------------------------- Baum-Welch --
 

main.py
@@ -3,38 +3,40 @@ import make
 import learn
 
 np.random.seed(42)
-N = 15
+N = 2000
 
 # ----------------------------------------------------------- Data generation --
 
-transition_matrix = make.transition()
-emission_matrix = make.emission()
-states = make.states(transition_matrix, N)
-values = make.values(states, emission_matrix)
+def main():
+    transition_matrix = make.transition()
+    emission_matrix = make.emission()
+    states = make.states(transition_matrix, N)
+    values = make.values(states, emission_matrix)
 
-print "Transition matrix: \n", transition_matrix
-print 
-print "Emission matrix: \n", log(emission_matrix)
-print
-print "States: ", states
-print
-print "Values: ", values + 1
-print
-print "States expected fraction: ", learn.ergodic_distribution(transition_matrix)
-print "States real fraction: ", learn.state_fractions(states)
-print
+    print "Transition matrix: \n", transition_matrix
+    print 
+    print "Emission matrix: \n", log(emission_matrix)
+    print
+    print "States: ", states
+    print
+    print "Values: ", values + 1
+    print
+    print "States expected fraction: ", learn.ergodic_distribution(transition_matrix)
+    print "States real fraction: ", learn.state_fractions(states)
+    print
 
-# -------------------------------------------------------- Viterbi and F.-B. --
+    # -------------------------------------------------------- Viterbi and F.-B. --
 
-states_vi = learn.states_viterbi(values, transition_matrix, emission_matrix)
-states_fb = learn.states_forward_backward(values, transition_matrix, emission_matrix)
+    states_vi = learn.states_viterbi(values, transition_matrix, emission_matrix)
+    states_fb = learn.states_forward_backward(values, transition_matrix, emission_matrix)
 
-print "-----------------------------------------------------------------------"
-print "Viterbi states: ", states_vi
-print
-print "Viterbi accuracy: ", mean(states_vi == states)
-print
-print "Fwd-Back states: ", states_fb
-print
-print "Fwd-Back accuracy: ", mean(states_fb == states)
+    print "-----------------------------------------------------------------------"
+    print "Viterbi states: ", states_vi
+    print
+    print "Viterbi accuracy: ", mean(states_vi == states)
+    print
+    print "Fwd-Back states: ", states_fb
+    print
+    print "Fwd-Back accuracy: ", mean(states_fb == states)
     
+main()